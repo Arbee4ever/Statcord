a68af2765b97d0157d3b16dbb757d8251a57961f
Arbee4ever <arbee@arbeeco.de>
2022-11-26 09:36:03 +0100

    Auth completed

--- src/main/java/de/arbeeco/statcord/api/Api.java
+++ src/main/java/de/arbeeco/statcord/api/Api.java
@@ -1,7 +1,9 @@
 package de.arbeeco.statcord.api;
 
 import com.google.gson.JsonObject;
+import com.iwebpp.crypto.TweetNaclFast;
 import de.arbeeco.statcord.StatcordBot;
+import de.arbeeco.statcord.util.Config;
 import io.javalin.Javalin;
 import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
@@ -17,50 +19,51 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.time.Instant;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class Api {
     public static DataApi dataApi;
     public static ConfigApi configApi;
-    private static JDA jda;
+    JDA jda;
 
     public Api(JDA jda) {
         this.jda = jda;
         dataApi = new DataApi(jda);
         configApi = new ConfigApi(jda);
         Javalin app = Javalin.create(config -> {
-                    config.plugins.enableCors(cors -> {
-                        cors.add(CorsPluginConfig::anyHost);
-                    });
-                    config.accessManager(((handler, context, set) -> {
-                        Role userRole = getUserRole(context);
-                        RouteRole routeRole = (RouteRole) set.toArray()[0];
-                        if (userRole.ordinal() >= Role.valueOf(routeRole.toString()).ordinal()) {
+                    config.plugins.enableCors(cors -> cors.add(CorsPluginConfig::anyHost));
+                    config.accessManager((handler, context, routeRoles) -> {
+                        if (routeRoles.size() == 0) {
+                            handler.handle(context);
+                            return;
+                        }
+                        Set<Permissions> userRoles = getUserRole(context);
+                        if (userRoles.containsAll(routeRoles)) {
                             handler.handle(context);
                         } else {
                             context.status(401).result("Unauthorized");
                         }
-                    }));
+                    });
                 })
                 .routes(() -> {
-                    get(ctx -> ctx.result("Hello World"), Role.EVERYONE);
+                    get(ctx -> ctx.result("Hello World"));
                     path("guilds", () -> {
-                        get(dataApi::getGuilds, Role.EVERYONE);
+                        get(dataApi::getGuilds);
                         path("{guildId}", () -> {
-                            before(this::beforeGuildId);
-                            get(dataApi::getGuildById, Role.GUILD_ADMIN);
-                            get("config", configApi::getGuildConfig, Role.GUILD_MODERATOR);
-                            patch("{category}", configApi::setGuildConfig, Role.GUILD_MODERATOR);
-                            get("{category}", configApi::getGuildConfigCategory, Role.GUILD_MODERATOR);
+                            get(dataApi::getGuildById);
+                            get("config", configApi::getGuildConfig, Permissions.GUILD_CONFIG);
+                            patch("{category}", configApi::setGuildConfig, Permissions.GUILD_CONFIG);
+                            get("{category}", configApi::getGuildConfigCategory, Permissions.GUILD_CONFIG);
                         });
                     });
                     path("user", () -> {
-                        get("{userId}", dataApi::getUser, Role.EVERYONE);
+                        get("{userId}", dataApi::getUser);
                     });
                 })
-                .before("/guilds/{guildId}/*", this::beforeGuildId)
-                .before("/guilds/{guildId}", this::beforeGuildId)
                 .start();
 
         app.exception(Exception.class, (exception, ctx) -> {
@@ -70,7 +73,7 @@ public class Api {
             jda.retrieveUserById(391979592883372042L).queue(user ->
                     user.openPrivateChannel()
                             .flatMap(privateChannel -> {
-                                PrintStream ps = null;
+                                PrintStream ps;
                                 try {
                                     ps = new PrintStream(file);
                                 } catch (FileNotFoundException e) {
@@ -80,40 +83,27 @@ public class Api {
                                 return privateChannel.sendMessage("Path: " + ctx.fullUrl() + "\nError: ```" + exception.getMessage() + "```").addFiles(FileUpload.fromData(file));
                             })
                             .queue());
-            file.delete();
+            if (!file.delete()) StatcordBot.logger.error("Logfile not deleted!");
             ctx.json(String.valueOf(errorResp));
         });
 
         StatcordBot.logger.info("API started: " + Date.from(Instant.now()));
     }
 
-    Role getUserRole(Context ctx) {
-        return Role.GUILD_ADMIN;
-    }
-
-    enum Role implements RouteRole {
-        EVERYONE, GUILD_USER, GUILD_MODERATOR, GUILD_ADMIN
-    }
-
-    public static boolean isAuthorized(Context ctx) {/*
-        Guild guild = jda.getGuildById(ctx.pathParam("guildId"));
-        if (Objects.equals(ctx.header("Authorization"), jda.getToken()) || Objects.equals(ctx.header("Authorization"), Config.getConfigValue(guild, "auth", "token")))
-            return true;
-        ctx.status(401);
-        JsonObject error = new JsonObject();
-        error.addProperty("message", "401: Unauthorized");
-        ctx.json(error.toString());
-        return false;*/
-        return true;
-    }
-
-    private void beforeGuildId(Context ctx) {
-        if (ctx.pathParam("guildId").isBlank() || !ctx.pathParam("guildId").matches("[0-9]+")) throw new BadRequestResponse("Please provide a valid Guild-ID");
-        if (ctx.pathParamAsClass("guildId", String.class).hasValue()) {
-            Guild guild = jda.getGuildById(ctx.pathParam("guildId"));
-            if (guild == null) {
-                throw new NotFoundResponse("Guild not found");
-            }
+    Set<Permissions> getUserRole(Context ctx) {
+        Guild guild = jda.getGuildById(ctx.pathParamAsClass("guildId", Long.class).getOrThrow(error -> new BadRequestResponse("Invalid Guild-ID")));
+        if (guild == null) {
+            throw new NotFoundResponse("Guild not found");
         }
+        Set<Permissions> userRoles = new HashSet<>();
+        if (Objects.equals(ctx.header("Authorization"), jda.getToken()) || Objects.equals(ctx.header("Authorization"), Config.getConfigValue(guild, "auth", "token"))) {
+            userRoles.add(Permissions.GUILD_CONFIG);
+            return userRoles;
+        }
+        return userRoles;
+    }
+
+    enum Permissions implements RouteRole {
+        GUILD_CONFIG
     }
 }
--- src/main/java/de/arbeeco/statcord/api/ConfigApi.java
+++ src/main/java/de/arbeeco/statcord/api/ConfigApi.java
@@ -1,23 +1,18 @@
 package de.arbeeco.statcord.api;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 import com.mongodb.client.FindIterable;
 import de.arbeeco.statcord.util.Config;
-import io.javalin.Javalin;
 import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
-import io.javalin.http.NotFoundResponse;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import org.bson.Document;
 
-import java.lang.reflect.MalformedParametersException;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-
-import static de.arbeeco.statcord.api.Api.isAuthorized;
-import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class ConfigApi {
     private final JDA jda;
@@ -27,18 +22,16 @@ public class ConfigApi {
     }
 
     public void getGuildConfig(Context ctx) {
-        if (!isAuthorized(ctx)) return;
         ctx.json(String.valueOf(getConfig(ctx)));
     }
 
     public void setGuildConfig(Context ctx) {
-        if (!isAuthorized(ctx)) return;
         try {
             JsonParser.parseString(ctx.body());
         } catch (JsonSyntaxException e) {
             throw new BadRequestResponse("Invalid JSON String");
         }
-        Guild guild = jda.getGuildById(ctx.pathParam("guildId"));
+        Guild guild = jda.getGuildById(ctx.pathParamAsClass("guildId", Long.class).getOrThrow(error -> new BadRequestResponse("Invalid Guild-ID")));
         String categoryName = ctx.pathParam("category");
         Map<String, Object> body = ctx.bodyAsClass(Map.class);
         body.forEach((name, value) -> {
@@ -49,15 +42,14 @@ public class ConfigApi {
     }
 
     public void getGuildConfigCategory(Context ctx) {
-        if (!isAuthorized(ctx)) return;
-        Guild guild = jda.getGuildById(ctx.pathParam("guildId"));
+        Guild guild = jda.getGuildById(ctx.pathParamAsClass("guildId", Long.class).getOrThrow(error -> new BadRequestResponse("Invalid Guild-ID")));
         Map data = Config.getConfigCategory(guild, ctx.pathParam("category"));
         ctx.json(String.valueOf(data));
         ctx.status(200);
     }
 
     private JsonObject getConfig(Context ctx) {
-        Guild guild = jda.getGuildById(ctx.pathParam("guildId"));
+        Guild guild = jda.getGuildById(ctx.pathParamAsClass("guildId", Long.class).getOrThrow(error -> new BadRequestResponse("Invalid Guild-ID")));
         FindIterable<Document> collection = Config.getGuildConfig(guild).find();
         JsonObject respObj = new JsonObject();
         for (Document document : collection) {
