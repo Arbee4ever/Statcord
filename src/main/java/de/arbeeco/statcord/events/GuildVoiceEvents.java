4b5a6183c005b4d6c17777bfe5bdc4116de4994a
Arbee4ever <robin.baenecke@mail.de>
2022-12-09 22:06:21 +0100

    implement role rewards

--- src/main/java/de/arbeeco/statcord/events/GuildVoiceEvents.java
+++ src/main/java/de/arbeeco/statcord/events/GuildVoiceEvents.java
@@ -1,6 +1,8 @@
 package de.arbeeco.statcord.events;
 
+import de.arbeeco.statcord.StatcordBot;
 import de.arbeeco.statcord.util.Data;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -9,9 +11,11 @@ public class GuildVoiceEvents extends ListenerAdapter {
     public void onGuildVoiceUpdate(GuildVoiceUpdateEvent event) {
         if (event.getMember().getUser().isBot()) return;
         if (event.getChannelJoined() != null && event.getChannelLeft() == null) {
-            Data.setVcStart(event.getGuild(), event.getMember());
+            Data.setVcStart(event.getMember());
         }
         if (event.getChannelLeft() != null && event.getChannelJoined() == null) {
+            User user = event.getMember().getUser();
+            StatcordBot.logger.info("Someone left VC! bot=" + user.isBot() + " Name#disciminator: " + user.getName() + "#" + user.getDiscriminator());
             Data.awardVcPoints(event.getGuild(), event.getMember());
         }
     }
