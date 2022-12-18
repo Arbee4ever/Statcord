d468100774e067311300fcbc4e3717fe1c6e212e
Arbee4ever <robin.baenecke@mail.de>
2022-12-18 23:08:31 +0100

    -fix leaderboard sorting

--- src/main/java/de/arbeeco/statcord/commands/LeaderboardCommand.java
+++ src/main/java/de/arbeeco/statcord/commands/LeaderboardCommand.java
@@ -1,6 +1,8 @@
 package de.arbeeco.statcord.commands;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Sorts;
 import de.arbeeco.statcord.util.Data;
 import de.arbeeco.statcord.util.StatcordEmbed;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -8,6 +10,10 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 import static com.mongodb.client.model.Indexes.descending;
 
@@ -19,7 +25,11 @@ public class LeaderboardCommand {
                 .setTitle("View full Leaderboard!", "https://statcord.arbeeco.de/leaderboards/" + event.getGuild().getId());
         String description = "";
         int count = 0;
-        for (Document memberData : collection.find().sort(descending("textscore", "voicescore")).limit(10)) {
+        FindIterable<Document> data = collection.find().sort(Sorts.descending("textmessages", "voiceseconds", "id")).limit(10);
+        List<Document> dataList = new ArrayList<>();
+        data.into(dataList);
+        dataList.sort(Comparator.comparingInt(el -> el.getInteger("textmessages") - el.getInteger("voiceseconds")));
+        for (Document memberData : dataList) {
             count++;
             Member  member = guild.getMemberById(memberData.get("id").toString());
             int txtscore = Data.getTextScore(member);
