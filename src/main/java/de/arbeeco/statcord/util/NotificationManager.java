64537ea85324d4d1970b4bc68325db92760b4a92
Arbee4ever <arbee@arbeeco.de>
2025-03-23 20:44:53 +0100

    chore: remove logging message

--- src/main/java/de/arbeeco/statcord/util/NotificationManager.java
+++ src/main/java/de/arbeeco/statcord/util/NotificationManager.java
@@ -35,7 +35,6 @@ public class NotificationManager {
             .uri(url)
             .POST(HttpRequest.BodyPublishers.ofString(bodyString))
             .build();
-    logger.info(bodyString);
     return client.send(request, HttpResponse.BodyHandlers.ofString());
   }
 }
