b69232f52fa77713b600215bc90d75ee4b200f73
Arbee4ever <arbee@arbeeco.de>
2025-03-23 21:36:36 +0100

    fix: show non zero-based shard id in Ping command

--- src/main/java/de/arbeeco/statcord/commands/slash/PingCommand.java
+++ src/main/java/de/arbeeco/statcord/commands/slash/PingCommand.java
@@ -4,12 +4,12 @@ import de.arbeeco.statcord.util.StatcordEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 public class PingCommand {
-    public PingCommand(SlashCommandInteractionEvent event) {
-        event.replyEmbeds(new StatcordEmbed()
-                .setDescription("Gateway Ping: **" + event.getJDA().getGatewayPing() + "**ms \n" +
-                        "API Ping: **" + event.getJDA().getRestPing().complete() + "**ms")
-                .setFooter("Shard: " + event.getJDA().getShardInfo().getShardId() + "/" + event.getJDA().getShardInfo().getShardTotal() + " - Guilds: " + event.getJDA().getShardManager().getGuilds().size())
-                .build()
-        ).queue();
-    }
+  public PingCommand(SlashCommandInteractionEvent event) {
+    event.replyEmbeds(new StatcordEmbed()
+            .setDescription("Gateway Ping: **" + event.getJDA().getGatewayPing() + "**ms \n" +
+                    "API Ping: **" + event.getJDA().getRestPing().complete() + "**ms")
+            .setFooter("Shard: " + (event.getJDA().getShardInfo().getShardId() + 1) + "/" + event.getJDA().getShardInfo().getShardTotal() + " - Guilds: " + event.getJDA().getShardManager().getGuilds().size())
+            .build()
+    ).queue();
+  }
 }
